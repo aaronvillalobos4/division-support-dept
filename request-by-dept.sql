81fb8c7d25fc1805c38ffaffcb512908a19ee33d
Aaron Villalobos <32658264+aaronvillalobos4@users.noreply.github.com>
2026-04-19 22:27:52 -0500

    Delete request-by-dept.sql

--- request-by-dept.sql
+++
@@ -1,6 +0,0 @@
-SELECT 
-  string_field_2 AS Dept_Type,
-  COUNT(string_field_2) AS Total_Requests
-FROM `division-support.dvision_support_all.division-support-table-all`
-GROUP BY Dept_Type
-ORDER BY Total_Requests DESC;
